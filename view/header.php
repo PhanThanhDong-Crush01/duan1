86c96a2a8f00ed9f08fa5e14dc592230b2f1d0f8
linh2411 <reika241103@gmail.com>
2022-11-10 21:21:35 +0700

    first commit

---
+++ view/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dự án mẫu</title>
+    <link rel="stylesheet" href="view/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row mb header">
+            <h1>THỜI TRANG MÙA ĐÔNG CHICOR</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <div class="l">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="index.php?act=gioithieu">Giới thiệu</a></li>
+                <li><a href="index.php?act=lienhe">Liên hệ</a></li>
+                <li><a href="index.php?act=gopy">Góp ý</a></li>
+                <li><a href="index.php?act=hoidap">Hỏi đáp</a></li></div>
+                <div class="svg"><a href="index.php?act=addtocart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16" color="white">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </svg></a></div>
+            </ul>
+        </div>
+<style>
+    .menu ul{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
